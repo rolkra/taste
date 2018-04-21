c01bd2e414b0b9ddbeaad1a7cf53a07df07fc8c2
rolkra <31808268+rolkra@users.noreply.github.com>
2018-04-21 08:25:11 +0200

    Update beer_test_explore_handson.R
    simplify

--- beer_test_explore_handson.R
+++ beer_test_explore_handson.R
@@ -2,9 +2,6 @@
 ## install packages (if required)
 ################################################################################
 
-# install tidyverse
-install.packages("tidyverse")
-
 # install from github
 if (!require(remotes)) install.packages("remotes")
 remotes::install_github("rolkra/explore")
@@ -15,7 +12,6 @@ remotes::install_github("rolkra/explore")
 
 # read data
 data <- read.csv("https://raw.githubusercontent.com/rolkra/taste/master/beer_test_data_all.csv")
-glimpse(data)
 
 # use explore package to explore data
 library(explore)
